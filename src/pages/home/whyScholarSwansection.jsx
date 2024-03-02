1851727e820e0b1d6e8956edd72e40c4a23d7fd3
Lokesh b s <84077131+lokesbs0608@users.noreply.github.com>
2024-03-02 14:40:45 +0530

    second section

--- src/pages/home/whyScholarSwansection.jsx
+++ src/pages/home/whyScholarSwansection.jsx
@@ -5,39 +5,46 @@ import Floating_books from "../../Images/Floating_books.png";
 
 const WhyScholarSwanSection = () => {
   return (
-    <div id="section2">
-      <div className="elem-pg2">
-        <div id="img-div">
-          <img src={web} alt="boy-img" />
+    <div id="section2" className="container py-4 mt-4">
+      <div className="row align-items-center justify-content-between">
+        <div className=" p-2 col-sm-12 col-md-5">
+          <div >
+            <div id="img-fluid w-100">
+              <img src={web} alt="boy-img" />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="elem-pg2 text-container">
-        <h1>
-          Why<span>scholarSWAN</span>
-        </h1>
-        <div id="text2">
-          <p>
-            At Scholar Swan, we're proud to offer a unique education approach.
-            Unlike other institutes, we provide both traditional and modern
-            support because we know every student is different. Our goal is to
-            improve learning by using innovative strategies, providing great
-            motivation, and offering expert counselling. We also have
-            well-designed tests for guaranteed results and parental
-            satisfaction. Our experienced mentors are here to guide you to
-            success. Choosing Scholar Swan means choosing an educational journey
-            where your hard work leads to exceptional results.
-          </p>
-          <div id="blur"></div>
-        </div>
+        <div className="p-2 col-sm-12 col-md-5">
+          <div >
+            <h1>
+              Why<span>scholarSWAN</span>
+            </h1>
+            <div className="text_">
+              <p>
+                At Scholar Swan, we're proud to offer a unique education
+                approach. Unlike other institutes, we provide both traditional
+                and modern support because we know every student is different.
+                Our goal is to improve learning by using innovative strategies,
+                providing great motivation, and offering expert counselling. We
+                also have well-designed tests for guaranteed results and
+                parental satisfaction. Our experienced mentors are here to guide
+                you to success. Choosing Scholar Swan means choosing an
+                educational journey where your hard work leads to exceptional
+                results.
+              </p>
+              <div id="blur"></div>
+            </div>
 
-        <HoverImageNavLink
-          to={"/online-tution"}
-          activeClassName="menu_active"
-          className="hover_arrow_btn"
-          sameColor={true}
-        >
-          Read More
-        </HoverImageNavLink>
+            <HoverImageNavLink
+              to={"/online-tution"}
+              activeClassName="menu_active"
+              className="hover_arrow_btn"
+              sameColor={true}
+            >
+              Read More
+            </HoverImageNavLink>
+          </div>
+        </div>
       </div>
 
       <img
